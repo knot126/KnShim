1c170206d3f3508a15e53f1d6f7fa3acc0ae8fd8
knot126 <73200930+knot126@users.noreply.github.com>
2024-10-27 03:00:18 +0000

    Basic install script

---
+++ install.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import argparse
+import shutil
+import os
+from pathlib import Path
+
+LIB_DIR = str(Path(__file__).parent) + "/libs"
+
+def install(path):
+	shutil.copytree(LIB_DIR, f"{path}/lib", dirs_exist_ok=True)
+	amx = Path(f"{path}/AndroidManifest.xml")
+	amx.write_bytes(amx.read_bytes().replace(b'android:name="android.app.lib_name" android:value="smashhit"', b'android:name="android.app.lib_name" android:value="shim"'))
+	
+	try:
+		import patcher
+		
+		try:
+			patcher.patch_binary(f"{path}/armeabi-v7a/libsmashhit.so", {"antitamper": []})
+			patcher.patch_binary(f"{path}/arm64-v8a/libsmashhit.so", {"antitamper": []})
+		except:
+			print("Warning: Failed to patch some binaries.")
+	except ImportError:
+		print("Note: The patcher could not be found, so you will need to patch libsmashhit.so against anti-tamper manually.")
+
+def main():
+	args = argparse.ArgumentParser()
+	args.add_argument("file", help="Install KnShim to an extracted Smash Hit APK directory")
+	args = args.parse_args()
+	
+	install(args.file)
+
+if __name__ == "__main__":
+	main()
