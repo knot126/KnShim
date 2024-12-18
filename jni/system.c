4dd1027c2c23a6e6a5685c0f1ad768ce2109a140
knot126 <73200930+knot126@users.noreply.github.com>
2024-12-18 21:21:08 +0000

    Release 10

--- jni/system.c
+++ jni/system.c
@@ -9,7 +9,7 @@ extern char *gAndroidInternalDataPath;
 extern char *gAndroidExternalDataPath;
 
 int knGetShimVersion(lua_State *script) {
-    lua_pushinteger(script, 9);
+    lua_pushinteger(script, 10);
     return 1;
 }
 
