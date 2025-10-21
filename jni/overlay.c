61e9899cf8f02c1240c2d6853e30ba9817b8469b
knot126 <cddepppp256@proton.me>
2025-10-21 03:01:43 +0000

    Don't look for '.mp3' files in overlay anymore

--- jni/overlay.c
+++ jni/overlay.c
@@ -19,162 +19,6 @@
 #include "util.h"
 #include "smashhit.h"
 
-// #define OLD_OVERLAYS
-
-#ifdef OLD_OVERLAYS
-// Zip reading related
-mz_zip_archive *gZip;
-
-bool mount_overlay(const char *path) {
-	/**
-	 * Attach an overlay zip so that any files in the zip will be loaded before
-	 * ones in the assets directory.
-	 */
-	
-	if (gZip) {
-		__android_log_print(ANDROID_LOG_ERROR, TAG, "Overlay is already mounted");
-		return false;
-	}
-	
-	// Alloc zip archive reader
-	gZip = malloc(sizeof *gZip);
-	
-	if (!gZip) {
-		__android_log_print(ANDROID_LOG_ERROR, TAG, "Could not allocate for zip reader");
-		return false;
-	}
-	
-	mz_zip_zero_struct(gZip);
-	
-	// Init zip reader
-	if (!mz_zip_reader_init_file(gZip, path, 0)) {
-		free(gZip);
-		gZip = NULL;
-		return false;
-	}
-	
-	__android_log_print(ANDROID_LOG_INFO, TAG, "Overlay initialised: %s", path);
-	
-	return true;
-}
-
-bool unmount_overlay(void) {
-	/**
-	 * Un-attach an overlay
-	 */
-	
-	if (!gZip) {
-		return false;
-	}
-	
-	mz_zip_reader_end(gZip);
-	free(gZip);
-	gZip = NULL;
-	
-	return true;
-}
-
-static size_t KNOverlayWriteFileCallback(void *pOpaque, mz_uint64 file_ofs, const void *pBuf, size_t n) {
-	return fwrite(pBuf, 1, n, (FILE *) pOpaque);
-}
-
-FILE *KNExtractOverlayToTempfile(mz_zip_archive *archive, const char *path, size_t *size_out) {
-	/**
-	 * Extract a file from the current overlay to a tempfile and return the file
-	 * handle.
-	 */
-	
-	int fileIndex = mz_zip_reader_locate_file(archive, path, NULL, MZ_ZIP_FLAG_CASE_SENSITIVE);
-	
-	if (fileIndex == -1) {
-		return NULL;
-	}
-	
-	mz_zip_archive_file_stat stat;
-	
-	if (!mz_zip_reader_file_stat(archive, fileIndex, &stat)) {
-		return NULL;
-	}
-	
-	*size_out = stat.m_uncomp_size;
-	
-	// TODO: Perhaps we should switch to using memfd_create()? fmemopen() just
-	// doesn't work, but perhaps this would since it creates a real file
-	// descriptor.
-	// SEE: https://www.man7.org/linux/man-pages/man2/memfd_create.2.html
-	FILE *file = tmpfile();
-	
-	if (!file) {
-		return NULL;
-	}
-	
-	// Extract data
-	if (!mz_zip_reader_extract_to_callback(archive, fileIndex, KNOverlayWriteFileCallback, file, 0)) {
-		fclose(file);
-		return NULL;
-	}
-	
-	fflush(file);
-	rewind(file);
-	
-	return file;
-}
-
-bool KNOverlayLoad(QiFileInputStream *this, const char *path) {
-	size_t size = 0;
-	
-	FILE *fi = KNExtractOverlayToTempfile(gZip, path, &size);
-	
-	if (fi) {
-		this->file = fi;
-		this->size = size;
-		this->position = 0;
-		this->androidAsset = NULL; // ignored if null
-		memset(&this->path, 0, sizeof this->path);
-	}
-	
-	return !!fi;
-}
-
-/**
- * ============================================================================
- * Lua functions
- * ============================================================================
- */
-
-int knMountOverlay(lua_State *script) {
-	if (lua_gettop(script) < 1) {
-		return 0;
-	}
-	
-	const char *path = lua_tostring(script, 1);
-	
-	if (!path) {
-		return 0;
-	}
-	
-	bool status = mount_overlay(path);
-	
-	lua_pushboolean(script, status);
-	
-	return 1;
-}
-
-int knUnmountOverlay(lua_State *script) {
-	lua_pushboolean(script, unmount_overlay());
-	
-	return 1;
-}
-
-int knEnableOverlay(lua_State *script) {
-	knRegisterFunc(script, knMountOverlay);
-	knRegisterFunc(script, knUnmountOverlay);
-	
-	return 0;
-}
-
-#else
-
 /**
  * A single, abstract overlay. The actual backing implementation may be a
  * physical directory, archive file, script callback or even something else but
@@ -519,7 +363,6 @@ int knEnableOverlay(lua_State *L) {
 	
 	return 0;
 }
-#endif
 
 /**
  * ============================================================================
@@ -534,25 +377,17 @@ bool QiFileInputStream_open_hook(QiFileInputStream *this, char *path) {
 	 * files from overlays before moving on to the assets directory.
 	 */
 	
-	// Try loading from overlay first
-#ifdef OLD_OVERLAYS
-	if (gZip) {
-#endif
-		if (KNOverlayLoad(this, path)) {
-			return true;
-		}
-		
-		// Also try loading without .mp3 (this is really needed ig...)
-		char path_no_mp3[strlen(path)+1];
-		strcpy(path_no_mp3, path);
-		path_no_mp3[strlen(path)-4] = '\0';
-		
-		if (KNOverlayLoad(this, path_no_mp3)) {
-			return true;
-		}
-#ifdef OLD_OVERLAYS
+	char final_path[strlen(path) + 1];
+	strcpy(final_path, path);
+	
+	// If the file path has the .mp3 suffix, we remove it.
+	if (strlen(final_path) >= 4 && !strcmp(final_path + strlen(final_path) - 4, ".mp3")) {
+		final_path[strlen(final_path) - 4] = '\0';
 	}
-#endif
+	
+	if (KNOverlayLoad(this, final_path)) {
+		return true;
+	}
 	
 	// Try real assets dir if that doesn't work
 	return QiFileInputStream_open(this, path);
