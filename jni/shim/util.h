e72a8e317aab075cd1be51ed0295c1de3eed6ef3
knot126 <cddepppp256@proton.me>
2025-11-10 19:05:25 +0000

    Move logging stuff to own header

--- jni/shim/util.h
+++ jni/shim/util.h
@@ -1,3 +1,29 @@
+/**
+ * Various utility functions that don't belong elsewhere.
+ * 
+ * -----------------------------------------------------------------------------
+ * 
+ * This file is part of KnShim. Copyright (c) 2024 - 2025 Knot126.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef _SHIM_UTIL_H
 #define _SHIM_UTIL_H
 
@@ -62,9 +88,6 @@ bool KNGetAppVersion(char *buffer, size_t maxSize);
 #define knReturnNil(SCRIPT) lua_pushnil(SCRIPT); return 1;
 #define KNLoadFunc(RET, NAME, SIG) RET (*NAME) SIG = KNGetSymbolAddr(#NAME);
 
-#define LogI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define LogW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
-#define LogE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
-#define LogF(...) __android_log_print(ANDROID_LOG_FATAL, TAG, __VA_ARGS__)
+#include "log.h"
 
 #endif // _SHIM_UTIL_H
